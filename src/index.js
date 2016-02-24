fae71a4f3c2cb8a715bc9d87cf030a75b7cfbcda
Dustin Specker <DustinSpecker@DustinSpecker.com>
2016-02-23 19:46:08 -0600

    chore(deps): update eslint
    Remove JSCS and JSHint.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-'use strict';
-import {EOL} from 'os';
-import newlineRegex from 'newline-regex';
-import ngModHasDep from 'ng-mod-has-dep';
+'use strict'
+import {EOL} from 'os'
+import newlineRegex from 'newline-regex'
+import ngModHasDep from 'ng-mod-has-dep'
 
 /**
  * Adds dependency to fileContents
@@ -11,46 +11,48 @@ import ngModHasDep from 'ng-mod-has-dep';
  */
 module.exports = function (fileContents, dependency) {
   // find line to add new dependency
-  let lines = fileContents.split(newlineRegex)
-    , angularDefinitionOpenLine = -1
+  const lines = fileContents.split(newlineRegex)
+  let angularDefinitionOpenLine = -1
     , angularDefinitionCloseLine = -1
-    , i, numOfSpaces;
+    , formattedDependency, i
 
   if (ngModHasDep(fileContents, dependency)) {
-    return fileContents;
+    return fileContents
   }
 
   lines.forEach((line, lineIndex) => {
     // find line with angular.module('*', [
     if (angularDefinitionOpenLine < 0 && line.indexOf('.module') > -1) {
-      angularDefinitionOpenLine = lineIndex;
+      angularDefinitionOpenLine = lineIndex
     }
 
-    // find line with closing ]);
+    // find line with closing ])
     if (angularDefinitionOpenLine > -1 && angularDefinitionCloseLine < 0 && line.indexOf(']') > -1) {
-      angularDefinitionCloseLine = lineIndex;
+      angularDefinitionCloseLine = lineIndex
     }
-  });
+  })
 
   // if there is a previous dependency
   // remove new line and add a comma to the previous depdendency
   // slice at the last quote to remove the varying line endings
   if (angularDefinitionCloseLine > angularDefinitionOpenLine + 1) {
     lines[angularDefinitionCloseLine - 1] =
-      lines[angularDefinitionCloseLine - 1].slice(0, lines[angularDefinitionCloseLine - 1].lastIndexOf(`'`));
-    lines[angularDefinitionCloseLine - 1] = lines[angularDefinitionCloseLine - 1] + `',`;
+      lines[angularDefinitionCloseLine - 1].slice(0, lines[angularDefinitionCloseLine - 1].lastIndexOf('\''))
+    lines[angularDefinitionCloseLine - 1] = `${lines[angularDefinitionCloseLine - 1]}',`
   }
 
-  numOfSpaces = lines[angularDefinitionCloseLine].substring(0, lines[angularDefinitionCloseLine].search(/[^ ]/)).length;
+  const numOfSpaces = lines[angularDefinitionCloseLine]
+    .substring(0, lines[angularDefinitionCloseLine]
+    .search(/[^ ]/)).length
 
-  dependency = `'${dependency}'`;
+  formattedDependency = `'${dependency}'`
 
   for (i = 0; i < numOfSpaces + 2; i++) {
-    dependency = ' ' + dependency;
+    formattedDependency = ` ${formattedDependency}`
   }
 
-  // insert new line and dependency
-  lines.splice(angularDefinitionCloseLine, 0, dependency);
+  // insert new line and formattedDependency
+  lines.splice(angularDefinitionCloseLine, 0, formattedDependency)
 
-  return lines.join(EOL);
-};
+  return lines.join(EOL)
+}
--- test.js
+++ test.js
@@ -1,44 +1,44 @@
 /* global describe, it */
-'use strict';
-import assert from 'assert';
-import {EOL} from 'os';
-import ngAddDep from './lib/';
+'use strict'
+import assert from 'assert'
+import {EOL} from 'os'
+import ngAddDep from './lib/'
 
 describe('ng-add-dep', () => {
-  let fileContents = [
+  const fileContents = [
     'angular\n',
     `  .module('module', [\r\n`,
-    '  ]);'
-  ].join('');
+    '  ])'
+  ].join('')
 
   it('should add test dep', () => {
-    let expected = [
+    const expected = [
       `angular${EOL}`,
       `  .module('module', [${EOL}`,
       `    'test'${EOL}`,
-      '  ]);'
-    ].join('');
-    assert(ngAddDep(fileContents, 'test') === expected);
-  });
+      '  ])'
+    ].join('')
+    assert(ngAddDep(fileContents, 'test') === expected)
+  })
 
   it('should add comma and new line delimeted deps', () => {
-    let expected = [
+    const expected = [
       `angular${EOL}`,
       `  .module('module', [${EOL}`,
       `    'test',${EOL}`,
       `    'test2'${EOL}`,
-      '  ]);'
-    ].join('');
-    assert(ngAddDep(ngAddDep(fileContents, 'test'), 'test2') === expected);
-  });
+      '  ])'
+    ].join('')
+    assert(ngAddDep(ngAddDep(fileContents, 'test'), 'test2') === expected)
+  })
 
   it('should not add exising dep', () => {
-    let expected = [
+    const expected = [
       `angular${EOL}`,
       `  .module('module', [${EOL}`,
       `    'test'${EOL}`,
-      '  ]);'
-    ].join('');
-    assert(ngAddDep(ngAddDep(fileContents, 'test'), 'test') === expected);
-  });
-});
+      '  ])'
+    ].join('')
+    assert(ngAddDep(ngAddDep(fileContents, 'test'), 'test') === expected)
+  })
+})
